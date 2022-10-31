a015755c37aa168329cf9e443d0106657d2fff19
Sébastien Gaya <sebastien.gaya@gmail.com>
2022-10-31 12:18:10 +0100

    wip index.html

--- source/assets/js/action/debug.js
+++ source/assets/js/action/debug.js
@@ -2,16 +2,6 @@ window.addEventListener("DOMContentLoaded", function () {
     var debugNotificationBtn = document.getElementById('debug-notification-btn');
         debugBannerBtn = document.getElementById('debug-banner-btn');
 
-    chrome.storage.local.get("signals").then(function (data) {
-        var signalsList = document.getElementById("signals-list"),
-            liNode;
-        data.signals.signals.forEach(function (item) {
-            liNode = document.createElement("li");
-            liNode.innerText = `${new Date(item.jour).toLocaleString("fr-FR", { dateStyle: 'short' })} : ${item.message}`;
-            signalsList.appendChild(liNode);
-        });
-    });
-
     debugNotificationBtn.addEventListener('click', function () {
         chrome.runtime.sendMessage('', {
             type: 'ecogestes-debug-notification',
