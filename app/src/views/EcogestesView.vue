2b9532ceb5d1bde32f147fc1eb10f7d88af35c01
Sébastien Gaya <sebastien.gaya@gmail.com>
2022-11-10 17:26:02 +0100

    multiplier in ecogestes

--- app/src/views/EcogestesView.vue
+++ app/src/views/EcogestesView.vue
@@ -7,10 +7,48 @@ export default {
         EcogesteCard
     },
 
+    computed: {
+      getMultiplier: function () {
+        const now = new Date(),
+          hour = now.getHours(),
+          isoDate = `${now.getFullYear()}-${(now.getMonth() + 1).toString().padStart(2, 0)}-${now.getDate().toString().padStart(2, 0)}`;
+
+        const highConsoMultiplier = (8 <= hour && hour < 13 || 18 <= hour && hour < 20) ? 1.5 : 1;
+
+        const currentDay = this.signals.find(item => {
+          return item.jour.indexOf(isoDate) !== -1;
+        });
+
+        if (!currentDay) {
+          return highConsoMultiplier;
+        }
+
+        const currentValue = currentDay.values.find(function (value) {
+          return value.pas === hour;
+        });
+
+        switch (currentValue.hvalue) {
+          case 3:
+            return highConsoMultiplier * 2;
+          case 2:
+            return highConsoMultiplier * 1.5;
+          default:
+            return highConsoMultiplier;
+        }
+      }
+    },
+
     data () {
         return {
-          ecogestes: state.ecogestes
+          ecogestes: state.ecogestes,
+          signals: []
         }
+    },
+
+  mounted() {
+      chrome.storage.local.get("signals").then(function (data) {
+        this.signals = data.signals.signals;
+      }.bind(this));
     }
 }
 </script>
@@ -21,7 +59,7 @@ export default {
     />
   <main>
     <div class="fr-mb-4w" v-for="(ecogeste, key) in ecogestes">
-      <EcogesteCard :ecogesteKey="key" :ecogeste="ecogeste" :credit="true" />
+      <EcogesteCard :ecogesteKey="key" :ecogeste="ecogeste" :credit="true" :multiplier="getMultiplier" />
     </div>
   </main>
 </template>
